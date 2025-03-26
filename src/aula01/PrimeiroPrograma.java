aa70b6477bc60c44836ec56989d8c8207d804b95
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2025-03-26 14:33:08 -0300

    added class 01

---
+++ src/aula01/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package aula01;
+
+public class PrimeiroPrograma {
+
+	public static void main(String [] args) {
+	
+		System.out.println("Olá Mundo!");
+		System.out.println("Orientação a Objetos 1");
+	}
+}
