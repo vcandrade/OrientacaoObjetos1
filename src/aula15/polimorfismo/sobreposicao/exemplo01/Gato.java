60911514fb3eb396f739d616a09a8d0ce214658d
Vinicius Camargo Andrade <vinicius.andrade1990@gmail.com>
2025-11-19 22:00:28 -0300

    added class 15

---
+++ src/aula15/polimorfismo/sobreposicao/exemplo01/Gato.java
@@ -0,0 +1,21 @@
+package aula15.polimorfismo.sobreposicao.exemplo01;
+
+public class Gato extends Mamifero {
+
+	public Gato(String nome, String raca) {
+
+		super(nome, raca);
+	}
+
+	@Override
+	public void emitirSom() {
+
+		System.out.println(this.nome + " está miando.");
+	}
+
+	public void brincar() {
+
+		System.out.println(this.nome + " está brincando.");
+	}
+
+}
